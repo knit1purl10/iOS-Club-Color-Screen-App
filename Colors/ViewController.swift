22ea3eca833a3ded74fdbcaaa61c2d459abe39fa
Olivia Hon <olivia.hon@gmail.com>
2018-10-23 18:34:26 -0400

    Initial Commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Olivia Hon on 10/23/18.
+//  Copyright © 2018 Olivia Hon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
